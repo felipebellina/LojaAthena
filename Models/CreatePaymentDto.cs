80b7edd05153e0e1ab5de24c7d5ccafe25ea0d89
Felipe Bellina <felipeluizb@hotmail.com>
2025-02-25 14:31:39 -0300

    Criando classe CreatePayment

---
+++ Models/CreatePaymentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LojaAthena.Models;
+
+public class CreatePaymentDto
+{
+    public string? Description { get; set; }
+    public int Installments { get; set; }
+    public string? Token { get; set; }
+    public PayerDto? Payer { get; set; }
+    public string? PaymentMethodId { get; set; }
+    public decimal TransactionAmount { get; set; }
+}
