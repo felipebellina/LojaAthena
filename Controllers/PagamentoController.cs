b1370a8981f76f72a5cca181a5a98c447f6aeb10
Felipe Bellina <felipeluizb@hotmail.com>
2025-02-26 11:50:58 -0300

    Criação controller do pagamento

---
+++ Controllers/PagamentoController.cs
@@ -0,0 +1,40 @@
+﻿using LojaAthena.Models;
+using LojaAthena.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace LojaAthena.Controllers;
+
+public class PagamentoController : Controller
+{
+    private readonly IMercadoPagoRepository _mercadoPagoRepository;
+
+    public PagamentoController(IMercadoPagoRepository mercadoPagoRepository)
+    {
+        _mercadoPagoRepository = mercadoPagoRepository;
+    }
+
+
+    [HttpPost]
+    public IActionResult CreatePayment([FromBody]CreatePaymentDto paymentDto)
+    {
+        if (paymentDto == null)
+        {
+            return BadRequest("Dados de pagamento não fornecidos.");
+        }
+
+        var response = _mercadoPagoRepository.CreatePayment(paymentDto);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Success()
+    {
+        return View();
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
